1854f634c4ae520667715aea2588ca02b7a39a95
DoanLy5120 <doanthaoly.hkqn@gmail.com>
2025-07-21 00:51:54 +0700

    test chi tiết

--- shop/src/api/axiosClient.js
+++ shop/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", 
+  baseURL: "https://web-production-c18cf.up.railway.app/api", 
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
--- shop/src/routes/index.js
+++ shop/src/routes/index.js
@@ -11,7 +11,7 @@ import Category from "../page/manager/Category/index";
 import Product from "../page/manager/Product/index";
 import Provider from "../page/manager/Provider/index";
 import InventoryCheckSheet from "../page/manager/InventoryCheckSheet/index";
-// import GoodsReceiptSheet from "../page/manager/GoodsReceiptSheet/index";
+import GoodsReceipt from "../page/manager/GoodsReceipt/index";
 
 const publicRoutes = [
   { path: "/", component: HomePage, layout: DefaultLayout },
@@ -34,7 +34,7 @@ const privateRoutes = [
   { path: "/manager/hang-hoa/san-pham", component: Product, layout: null },
   { path: "/manager/giao-dich/nha-cung-cap", component: Provider, layout: ManagerLayout },
   { path: "/manager/hang-hoa/kiem-kho", component: InventoryCheckSheet, layout: ManagerLayout},
-  // { path: "/manager/giao-dich/nhap-hang", component: GoodsReceiptSheet, layout: ManagerLayout},
+  { path: "/manager/giao-dich/nhap-hang", component: GoodsReceipt, layout: ManagerLayout},
 ];
 
 export { publicRoutes, privateRoutes };
